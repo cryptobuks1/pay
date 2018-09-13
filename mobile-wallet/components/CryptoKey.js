4f9cdbc6057dbcf3fd0c8622af212d2acff21f69
Gentrion <gentlemars09@gmail.com>
2018-09-13 11:50:39 +0900

    2018.09.13.02

--- mobile-wallet/components/CryptoKey.js
+++ mobile-wallet/components/CryptoKey.js
@@ -1,22 +1,13 @@
-//const crypto = require('crypto');
 import CryptoJS from 'crypto-js';
 
 const nearKey = 'require(crypto)';
 
 function encryptKey(text){
   const ciphertext = CryptoJS.AES.encrypt(text, nearKey);
-  // const cipher = crypto.createCipher('aes-256-cbc',nearKey); 
-  // let encipheredContent = cipher.update(text,'utf8','hex'); 
-  // encipheredContent += cipher.final('hex');
-  // return encipheredContent;
   return ciphertext.toString();
 };
    
 function decryptKey(text){
-//   const decipher = crypto.createDecipher('aes-256-cbc',nearKey);
-//   let decipheredPlaintext = decipher.update(text,'hex','utf8');
-//   decipheredPlaintext += decipher.final('utf8');
-//   return decipheredPlaintext;
   let bytes  = CryptoJS.AES.decrypt(text, nearKey);
   return bytes.toString(CryptoJS.enc.Utf8);
 }
